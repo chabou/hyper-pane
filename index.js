6a9ece02358667a48ae6a7061497d34b99e3b290
CHaBou <chabup@delean.fr>
2017-03-25 15:13:48 +0100

    Add some config test to prevent some errors

--- index.js
+++ index.js
@@ -425,8 +425,8 @@ exports.decorateTerms = (Terms, { React, notify, Notification }) => {
       const document = term.getTermDocument();
       const keys = new Mousetrap(document);
 
-      const jump_prefix = config.hotkeys.jump_prefix.toLowerCase();
-      const permutation_modifier = config.hotkeys.permutation_modifier.toLowerCase();
+      const jump_prefix = config.hotkeys.jump_prefix ? config.hotkeys.jump_prefix.toLowerCase() : '';
+      const permutation_modifier = config.hotkeys.permutation_modifier ? config.hotkeys.permutation_modifier.toLowerCase() : '';
       if (jump_prefix && jump_prefix.length) {
         ['1','2','3','4','5','6','7','8','9'].forEach(num => {
           let shortcut = jump_prefix+ `+${num}`;
