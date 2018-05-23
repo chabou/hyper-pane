chabou/hyper-pane

commit 965bb3bc2f291459cd18632aa8e1bc39b8853442

fix: prevent maximize if there is only one pane
Fixes #27

index.js
@@ -276,6 +276,10 @@ const onMaximizePane = dispatch => () => {
       debug('No termGroup found for active Session');
       return;
     }
+    if (!termGroup.parentUid) {
+      debug('No parent for this session, maximize discarded');
+      return;
+    }
     dispatch_({
       type: 'UI_MAXIMIZE_PANE',
       uid: termGroup.uid

commit b0de17f914673d0ca599367711b6f63751645ec0

fix(maximize): fix unmaximize regression
Regression introduced by 965bb3bc2f291459cd18632aa8e1bc39b8853442

index.js
@@ -276,10 +276,6 @@ const onMaximizePane = dispatch => () => {
       debug('No termGroup found for active Session');
       return;
     }
-    if (!termGroup.parentUid) {
-      debug('No parent for this session, maximize discarded');
-      return;
-    }
     dispatch_({
       type: 'UI_MAXIMIZE_PANE',
       uid: termGroup.uid
@@ -343,6 +339,10 @@ exports.reduceTermGroups = (state, action) => {
           // Maximize
           debug('Maximizing', action.uid);
           const {parentUid, sessionUid} = state.termGroups[action.uid];
+          if (!parentUid) {
+            debug('No parent for this session, maximize discarded');
+            break;
+          }
           state = state.setIn(['maximizeSave', action.uid], {
             activeRootGroup: state.activeRootGroup,
             parentUid,